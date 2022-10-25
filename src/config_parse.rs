b0ed17ab5bc8bffad4ca264b1a75edbe06ffd4fd
egidius <egidius@mysliwietz.de>
2022-10-25 22:31:03 +0200

    Chance and Count Range properties, more prayers

--- src/config_parse.rs
+++ src/config_parse.rs
@@ -2,13 +2,19 @@ use std::fs::read_to_string;
 
 use chrono::Datelike;
 use linked_hash_map::LinkedHashMap;
-use rand::{rngs::StdRng, seq::SliceRandom, SeedableRng};
+use rand::{rngs::StdRng, seq::SliceRandom, Rng, SeedableRng};
 use yaml_rust::{Yaml, YamlLoader};
 
 use crate::prayer::{Prayer, _Prayer};
 
 const E: &str = "Malformed YAML";
 type PrayerList = Vec<Box<dyn Prayer>>;
+
+pub fn test() {
+    let s = read_to_string("preces/.config.yaml").unwrap();
+    let y: Vec<Yaml> = YamlLoader::load_from_str(&s).unwrap();
+    println!("{:?}", y[0]["order"])
+}
 
 /// Return a list of all prayer set titles and corresponding YAML
 pub fn get_all_prayset_titles() -> Vec<(String, Yaml)> {
@@ -68,29 +74,79 @@ pub fn process_group(y: &Yaml, g: &LinkedHashMap<Yaml, Yaml>) -> PrayerList {
 
 pub fn expand_group_definition(y: &Yaml, g: &str) -> PrayerList {
     let mut order: PrayerList = vec![];
-    for prayer in y["prayers"][g].as_vec().expect(E) {
-        order.push(Box::new(_Prayer::new(
-            prayer.as_str().expect(E).to_string(),
-        )))
+    let prayers = &y["prayers"][g];
+    if !prayers.is_badvalue() {
+        for prayer in prayers.as_vec().expect(E) {
+            order.push(Box::new(_Prayer::new(
+                prayer.as_str().expect(E).to_string(),
+            )))
+        }
+    } else {
+        order.push(Box::new(_Prayer::new(g.to_string())))
     }
     order
 }
 
+struct Properties {
+    /// Select a random number between count_min and count_max of Prayers
+    count_min: usize,
+    count_max: usize,
+    /// Whether to select prayers at random from group
+    random: bool,
+    /// Chance (in percent) to select any prayers from group at all
+    chance: i64,
+}
+
+fn get_properties(group: &PrayerList, properties: &Yaml) -> Properties {
+    let mut count_min = group.len();
+    let mut count_max = count_min;
+    let _count = properties["count"].as_i64();
+    if _count.is_some() {
+        count_min = _count.unwrap() as usize;
+        count_max = _count.unwrap() as usize;
+    } else {
+        let _count = properties["count"].as_str();
+        if _count.is_some() {
+            let _count: Vec<&str> = _count.unwrap().split("-").collect();
+            count_min = _count[0].parse().expect(E);
+            count_max = _count[1].parse().expect(E);
+        }
+    }
+
+    //.unwrap_or(group.len() as i64) as usize;
+    let random = properties["random"].as_bool().unwrap_or(false);
+    let chance = properties["chance"].as_i64().unwrap_or(100);
+
+    Properties {
+        count_min,
+        count_max,
+        random,
+        chance,
+    }
+}
+
 pub fn pick_and_apply_properties<'a>(group: &PrayerList, properties: &Yaml) -> PrayerList {
     let mut order: PrayerList = vec![];
-    let count = properties["count"].as_i64().unwrap_or(group.len() as i64) as usize;
-    let random = properties["random"].as_bool().unwrap_or(false);
-    if random {
-        let today = chrono::offset::Local::now()
-            .date()
-            .naive_local()
-            .num_days_from_ce() as u64;
-        let mut rng = StdRng::seed_from_u64(today);
-        for p in group.choose_multiple(&mut rng, count) {
-            order.push(p.clone());
-        }
-    } else {
-        order.append(&mut group[0..count].to_vec());
+    let p = get_properties(group, properties);
+
+    let today = chrono::offset::Local::now()
+        .date()
+        .naive_local()
+        .num_days_from_ce() as u64;
+    let mut rng = StdRng::seed_from_u64(today);
+
+    if !rng.gen_bool(p.chance as f64 / 100.0) {
+        return order;
+    }
+
+    let count: usize = rng.gen_range(p.count_min, p.count_max + 1);
+
+    for i in 0..count {
+        order.push(if p.random {
+            group.choose(&mut rng).expect(E).clone()
+        } else {
+            group[i & group.len()].clone()
+        })
     }
     order
 }
